bd1ea1514fb2cede79f887e2ee837dd71b074476
cozej4 <ilakozejumanne@gmail.com>
2020-06-02 11:46:15 +0300

    Implemented passing of fp_method while loading of give family planning method

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/PathfinderFamilyPlanningMemberProfileActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/PathfinderFamilyPlanningMemberProfileActivity.java
@@ -11,6 +11,10 @@ import androidx.annotation.NonNull;
 import com.adosa.opensrp.chw.fp.domain.PathfinderFpMemberObject;
 import com.adosa.opensrp.chw.fp.util.PathfinderFamilyPlanningConstants;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.BuildConfig;
 import org.smartregister.chw.R;
@@ -18,6 +22,8 @@ import org.smartregister.chw.core.activity.CorePathfinderFamilyPlanningMemberPro
 import org.smartregister.chw.core.interactor.CorePathfinderFamilyPlanningProfileInteractor;
 import org.smartregister.chw.core.utils.CoreConstants;
 import org.smartregister.chw.core.utils.PathfinderFamilyPlanningUtil;
+import org.smartregister.chw.fp.util.FamilyPlanningConstants;
+import org.smartregister.chw.fp.util.FpJsonFormUtils;
 import org.smartregister.chw.model.ReferralTypeModel;
 import org.smartregister.chw.presenter.PathfinderFamilyPlanningMemberProfilePresenter;
 import org.smartregister.commonregistry.CommonPersonObject;
@@ -25,6 +31,7 @@ import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.commonregistry.CommonRepository;
 import org.smartregister.family.util.Utils;
 import org.smartregister.util.FormUtils;
+import org.smartregister.util.JsonFormUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -136,7 +143,8 @@ public class PathfinderFamilyPlanningMemberProfileActivity extends CorePathfinde
 
     @Override
     public void openGiveFpMethodButton() {
-        PathfinderFamilyPlanningRegisterActivity.startFpRegistrationActivity(this, pathfinderFpMemberObject.getBaseEntityId(), pathfinderFpMemberObject.getAge(), CoreConstants.JSON_FORM.getPathfinderGiveFamilyPlanningMethod(), PathfinderFamilyPlanningConstants.ActivityPayload.CHANGE_METHOD_PAYLOAD_TYPE);
+        Timber.e("Coze::openGiveFpMethodButton "+pathfinderFpMemberObject.getFpMethod());
+        PathfinderFamilyPlanningRegisterActivity.startFpRegistrationActivity(this, pathfinderFpMemberObject.getBaseEntityId(), pathfinderFpMemberObject.getFpMethod(), CoreConstants.JSON_FORM.getPathfinderGiveFamilyPlanningMethod(), PathfinderFamilyPlanningConstants.ActivityPayload.GIVE_FP_METHOD);
     }
 
     @Override
