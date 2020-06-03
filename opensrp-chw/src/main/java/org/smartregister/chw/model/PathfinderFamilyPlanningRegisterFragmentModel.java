25d2a67a46a3b21e5824f82262968e80cb543eba
cozej4 <ilakozejumanne@gmail.com>
2020-06-03 23:08:10 +0300

    Code refactoring

--- opensrp-chw/src/main/java/org/smartregister/chw/model/PathfinderFamilyPlanningRegisterFragmentModel.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/model/PathfinderFamilyPlanningRegisterFragmentModel.java
@@ -14,7 +14,7 @@ public class PathfinderFamilyPlanningRegisterFragmentModel extends BaseFpRegiste
     @Override
     public String mainSelect(@NonNull String tableName, @NonNull String mainCondition) {
         SmartRegisterQueryBuilder queryBuilder = new SmartRegisterQueryBuilder();
-        queryBuilder.SelectInitiateMainTable(tableName, mainColumns(tableName));
+        queryBuilder.selectInitiateMainTable(tableName, mainColumns(tableName));
         queryBuilder.customJoin("INNER JOIN " + PathfinderFamilyPlanningConstants.DBConstants.FAMILY_MEMBER + " ON  " + tableName + "." + PathfinderFamilyPlanningConstants.DBConstants.BASE_ENTITY_ID + " = " + PathfinderFamilyPlanningConstants.DBConstants.FAMILY_MEMBER + "." + PathfinderFamilyPlanningConstants.DBConstants.BASE_ENTITY_ID + " COLLATE NOCASE ");
         queryBuilder.customJoin("INNER JOIN " + PathfinderFamilyPlanningConstants.DBConstants.FAMILY + " ON  " + PathfinderFamilyPlanningConstants.DBConstants.FAMILY_MEMBER + "." + PathfinderFamilyPlanningConstants.DBConstants.RELATIONAL_ID + " = " + PathfinderFamilyPlanningConstants.DBConstants.FAMILY + "." + PathfinderFamilyPlanningConstants.DBConstants.BASE_ENTITY_ID);
         queryBuilder.customJoin("LEFT JOIN (select base_entity_id , max(visit_date) visit_date from visits GROUP by base_entity_id) VISIT_SUMMARY ON VISIT_SUMMARY.base_entity_id = " + tableName + "." + DBConstants.KEY.BASE_ENTITY_ID);
