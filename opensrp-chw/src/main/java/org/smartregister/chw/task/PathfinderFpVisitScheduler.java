9888078d42ef8b8838af0b58be90d7fc01872320
cozej4 <ilakozejumanne@gmail.com>
2020-11-17 02:26:21 +0300

    Fixed pregnancy related scheduling issues

--- opensrp-chw/src/main/java/org/smartregister/chw/task/PathfinderFpVisitScheduler.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/task/PathfinderFpVisitScheduler.java
@@ -11,14 +11,17 @@ import org.smartregister.chw.core.contract.ScheduleTask;
 import org.smartregister.chw.core.domain.BaseScheduleTask;
 import org.smartregister.chw.core.rule.PathfinderFpAlertRule;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.FpUtil;
 import org.smartregister.chw.core.utils.PathfinderFamilyPlanningUtil;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import timber.log.Timber;
+
+import static com.adosa.opensrp.chw.fp.util.PathfinderFamilyPlanningConstants.EventType.FAMILY_PLANNING_PREGNANCY_SCREENING;
+import static com.adosa.opensrp.chw.fp.util.PathfinderFamilyPlanningConstants.EventType.FAMILY_PLANNING_PREGNANCY_TEST_REFERRAL_FOLLOWUP;
 
 public class PathfinderFpVisitScheduler extends BaseTaskExecutor {
 
@@ -30,24 +33,24 @@ public class PathfinderFpVisitScheduler extends BaseTaskExecutor {
 
     @Override
     public List<ScheduleTask> generateTasks(String baseEntityID, String eventName, Date eventDate) {
-        PathfinderFpMemberObject pathfinderFpMemberObject = PathfinderFpDao.getMember(baseEntityID);
-        String fpMethod = pathfinderFpMemberObject.getFpMethod();
-        String fp_date = pathfinderFpMemberObject.getFpStartDate();
-        Integer fp_pillCycles = 1;
-        Rules rule = PathfinderFamilyPlanningUtil.getFpRules(pathfinderFpMemberObject.getFpMethod());
-        BaseScheduleTask baseScheduleTask = prepareNewTaskObject(baseEntityID);
+        String fpMethod;
+        String fp_date = null;
+        Integer fp_pillCycles = null;
+        Rules rule = null;
+        Date serviceDueDate = null;
+        Date serviceOverDueDate = null;
+        Date serviceExpiryDate = null;
+        Date serviceCompletionDate = null;
+        String serviceName = null;
+        PathfinderFpMemberObject pathfinderFpAlertObject = PathfinderFpDao.getMember(baseEntityID);
 
-        if (fp_date == null || fp_date.isEmpty())
-            return new ArrayList<>();
-        Date lastVisitDate = null;
+        fpMethod = pathfinderFpAlertObject.getFpMethod();
+        fp_date = pathfinderFpAlertObject.getFpStartDate();
+        fp_pillCycles = PathfinderFpDao.getLastPillCycle(baseEntityID, fpMethod);
+        rule = PathfinderFamilyPlanningUtil.getFpRules(fpMethod);
+
+        Date lastVisitDate;
         Visit lastVisit;
-        lastVisit = PathfinderFpDao.getLatestFpVisit(baseEntityID);
-        if (lastVisit == null) {
-            lastVisit = PathfinderFpDao.getLatestFpVisit(baseEntityID, PathfinderFamilyPlanningConstants.EventType.FAMILY_PLANNING_REGISTRATION, fpMethod);
-        }
-        if (lastVisit != null) {
-            lastVisitDate = lastVisit.getDate();
-        }
 
         Date fpDate = null;
         try {
@@ -64,12 +67,46 @@ public class PathfinderFpVisitScheduler extends BaseTaskExecutor {
             }
         }
 
-        PathfinderFpAlertRule fpAlertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(rule, lastVisitDate, fpDate, fp_pillCycles, pathfinderFpMemberObject.getFpMethod());
+        lastVisit = PathfinderFpDao.getLatestFpVisit(baseEntityID, PathfinderFamilyPlanningConstants.EventType.FP_FOLLOW_UP_VISIT, fpMethod);
+        if (lastVisit == null) {
+            lastVisit = PathfinderFpDao.getLatestFpVisit(baseEntityID, PathfinderFamilyPlanningConstants.EventType.GIVE_FAMILY_PLANNING_METHOD, fpMethod);
+        }
 
-        baseScheduleTask.setScheduleDueDate(fpAlertRule.getDueDate());
-        baseScheduleTask.setScheduleExpiryDate(fpAlertRule.getExpiryDate());
-        baseScheduleTask.setScheduleCompletionDate(fpAlertRule.getCompletionDate());
-        baseScheduleTask.setScheduleOverDueDate(fpAlertRule.getOverDueDate());
+        if (lastVisit == null) {
+            lastVisit = PathfinderFpDao.getLatestFpVisit(baseEntityID);
+        }
+        lastVisitDate = lastVisit.getDate();
+        PathfinderFpAlertRule alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(rule, lastVisitDate, fpDate, fp_pillCycles, fpMethod);
+        if (!pathfinderFpAlertObject.getFpStartDate().equals("0") && !pathfinderFpAlertObject.getFpStartDate().equals("") && !fpMethod.isEmpty()) {
+
+        } else if (!pathfinderFpAlertObject.getEdd().isEmpty() && pathfinderFpAlertObject.getPregnancyStatus().equals(PathfinderFamilyPlanningConstants.PregnancyStatus.PREGNANT)) {
+            lastVisit = PathfinderFpDao.getLatestVisit(baseEntityID, FAMILY_PLANNING_PREGNANCY_SCREENING + "' OR visit_type = '" + FAMILY_PLANNING_PREGNANCY_TEST_REFERRAL_FOLLOWUP);
+            lastVisitDate = lastVisit.getDate();
+            alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(PathfinderFamilyPlanningUtil.getPregnantWomenFpRules(), lastVisitDate, FpUtil.parseFpStartDate(pathfinderFpAlertObject.getEdd()), fp_pillCycles, fpMethod);
+        } else if (pathfinderFpAlertObject.getPregnancyStatus().equals(PathfinderFamilyPlanningConstants.PregnancyStatus.NOT_UNLIKELY_PREGNANT)) {
+            lastVisit = PathfinderFpDao.getLatestVisit(baseEntityID, FAMILY_PLANNING_PREGNANCY_SCREENING + "' OR visit_type = '" + FAMILY_PLANNING_PREGNANCY_TEST_REFERRAL_FOLLOWUP);
+            lastVisitDate = lastVisit.getDate();
+
+            if (pathfinderFpAlertObject.getChoosePregnancyTestReferral().equals(PathfinderFamilyPlanningConstants.ChoosePregnancyTestReferral.WAIT_FOR_NEXT_VISIT)) {
+                alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(PathfinderFamilyPlanningUtil.getPregnantScreeningFollowupRules(), lastVisitDate, lastVisitDate, 0, fpMethod);
+            } else if (pathfinderFpAlertObject.isClientIsCurrentlyReferred()) {
+                alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(PathfinderFamilyPlanningUtil.getPregnantTestReferralFollowupRules(), lastVisitDate, lastVisitDate, 0, fpMethod);
+            }
+        } else if (pathfinderFpAlertObject.isManRequestedMethodForPartner()) {
+            rule = PathfinderFamilyPlanningUtil.getManChosePartnersFpMethodFollowupRules();
+
+            alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(rule, lastVisitDate, FpUtil.parseFpStartDate(pathfinderFpAlertObject.getFpMethodChoiceDate()), fp_pillCycles, fpMethod);
+        } else if (pathfinderFpAlertObject.isClientIsCurrentlyReferred()) {
+            rule = PathfinderFamilyPlanningUtil.getReferralFollowupRules();
+            alertRule = PathfinderFamilyPlanningUtil.getFpVisitStatus(rule, lastVisitDate, FpUtil.parseFpStartDate(pathfinderFpAlertObject.getFpStartDate()), fp_pillCycles, fpMethod);
+        }
+
+
+        BaseScheduleTask baseScheduleTask = prepareNewTaskObject(baseEntityID);
+        baseScheduleTask.setScheduleDueDate(alertRule.getDueDate());
+        baseScheduleTask.setScheduleExpiryDate(alertRule.getExpiryDate());
+        baseScheduleTask.setScheduleCompletionDate(alertRule.getCompletionDate());
+        baseScheduleTask.setScheduleOverDueDate(alertRule.getOverDueDate());
 
         return toScheduleList(baseScheduleTask);
     }
